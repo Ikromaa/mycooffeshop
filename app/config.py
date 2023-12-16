f7984f2b5b13f9a689f80f3d40d11dd63320e2eb
ASUS <amorki.hataf123@gmail.com>
2023-12-16 14:22:18 +0700

    dd

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-SECRET_KEY = 'your_secret_key'
+SECRET_KEY = '123456789'
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:dinar123@localhost/choffeshop'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
