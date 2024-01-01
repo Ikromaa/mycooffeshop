94e83f00df7324c72c66ef12e95807a86c011d07
ASUS <amorki.hataf123@gmail.com>
2024-01-01 18:16:29 +0700

    hapus migrasi

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from app import app, db, login_manager
 from app.forms import ReservationForm, RegistrationForm, LoginForm
-from app.models import User, Reservation, Admin
+from app.models import User, Reservation
 
 # Home page
 @app.route('/')
