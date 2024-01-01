348facd646aff2ba140e9cee74b4bd579d0bf62f
ASUS <amorki.hataf123@gmail.com>
2024-01-01 18:12:37 +0700

    admin

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,13 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from app import app, db, login_manager
 from app.forms import ReservationForm, RegistrationForm, LoginForm
-from app.models import User, Reservation
+from app.models import User, Reservation, Admin
 
 # Home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import app
 
 if __name__ == '__main__':
+    # create_admin()
     app.run(debug=True)
